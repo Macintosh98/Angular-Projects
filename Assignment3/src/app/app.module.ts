2ae26db1ee6fccf407a3712a6db9052367ea561e
Macintosh98 <abhishekzambare@gmail.com>
2018-06-23 11:49:53 +0530

    added component comp1 and comp2

--- Assignment3/src/app/app.module.ts
+++ Assignment3/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Comp1Component } from './comp1/comp1.component';
+import { Comp2Component } from './comp2/comp2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Comp1Component,
+    Comp2Component
   ],
   imports: [
     BrowserModule
